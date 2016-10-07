91db866553a0712c4c2c8e7cab26c766c38ded84
Michal Pokorný <prvak.cnx+git@gmail.com>
2016-10-07 12:28:57 +0200

    Use my key

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,8 @@
 
 bundle exec jekyll build
 
-s3cmd sync --delete-removed --recursive _site/* s3://www.eagxprague.org/
+if [ "$USER" == prvak ]; then
+	s3cmd -c ~/dropbox/hesla/ea-cr/s3cfg-eagxprague.org sync --delete-removed --recursive _site/* s3://www.eagxprague.org/
+else
+	s3cmd sync --delete-removed --recursive _site/* s3://www.eagxprague.org/
+fi
